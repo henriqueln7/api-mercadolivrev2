ab4ef5f4fd646d547476f41b7276a06da37abb24
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2021-01-03 18:08:03 -0300

    Initial commit

---
+++ src/test/java/com/mercadolivre/apimlv2/Apimlv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.apimlv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apimlv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
