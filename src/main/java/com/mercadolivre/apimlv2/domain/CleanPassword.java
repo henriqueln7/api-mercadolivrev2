c21c2d9bdc78b7bc63c2a6588e3663ff8824e819
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2021-01-04 09:08:04 -0300

    :recycle: Move hash function to inside CleanPassword class.

--- src/main/java/com/mercadolivre/apimlv2/domain/CleanPassword.java
+++ src/main/java/com/mercadolivre/apimlv2/domain/CleanPassword.java
@@ -1,5 +1,8 @@
 package com.mercadolivre.apimlv2.domain;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.Assert;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -9,10 +12,12 @@ public class CleanPassword {
     private final String password;
 
     public CleanPassword(@NotBlank @Size(min = 6) String password) {
+        Assert.hasText(password, "Password should not be blank");
+        Assert.isTrue(password.length() >= 6, "Password should have size >= 6");
         this.password = password;
     }
 
-    public String getPassword() {
-        return password;
+    public String hash() {
+        return new BCryptPasswordEncoder().encode(this.password);
     }
 }
--- src/main/java/com/mercadolivre/apimlv2/domain/User.java
+++ src/main/java/com/mercadolivre/apimlv2/domain/User.java
@@ -1,14 +1,10 @@
 package com.mercadolivre.apimlv2.domain;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
+@Table(name = "users")
 @Entity
 public class User {
     @Id
@@ -28,7 +24,7 @@ public class User {
     protected User(){}
 
     public User(@NotBlank @Email String login, CleanPassword password) {
-        this.password = new BCryptPasswordEncoder().encode(password.getPassword());
+        this.password = password.hash();
         this.login = login;
         this.createdAt = LocalDate.now();
     }
